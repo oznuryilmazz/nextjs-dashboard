1d1622fcf8dee356f722f82064a97b9e20399cf1
Öznur Yılmaz <yilmazoznur53@gmail.com>
2024-01-23 14:50:47 +0300

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': false },
-    },
+    autoprefixer: {},
   },
 };
